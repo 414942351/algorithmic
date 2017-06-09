da78754705d70b8e0f237e0f77fbc74073f9f214
yanbing <bing.yan@daigj.com>
2017-06-09 10:18:00 +0800

    ok

--- demo/demo20.js
+++ demo/demo20.js
@@ -1,1 +1,17 @@
 // Create all permutation of a string?
+// var str = 'abc';
+var arr = ['a', 'b', 'c', 'd'];
+function permutation(arr) {
+	var len = arr.length;
+	var ret = [];
+	var arrtemp = arr.splice(0, 1);
+	for(var i = 0; i < arrtemp.length; i++) {
+		// arr[i] = per[i];
+		// console.log(arr);
+
+	}
+}
+permutation(arr);
+
+// var obj = Object.create(arr);
+// console.log(obj.__proto__);
